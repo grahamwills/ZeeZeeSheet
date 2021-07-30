e6d7faa9e6c8821abab0fda7398384625b99e10f
graham <graham@willsfamily.org>
2021-07-29 19:20:55 -0500

    Suupport for check boxes

--- sheet/pdf.py
+++ sheet/pdf.py
@@ -1,15 +1,21 @@
 from typing import Dict
 
+import reportlab.lib.colors
 from colour import Color
 from reportlab.lib import pagesizes
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfgen import canvas
+from reportlab.pdfgen.textobject import PDFTextObject
 from reportlab.platypus import Paragraph
 
-from common import Rect
+from common import Rect, configured_logger
 from model import Element, ElementType, Run, Style
 
+LOGGER = configured_logger(__name__)
+
+_CHECKED_BOX = '../data/images/system/checked.png'
+_UNCHECKED_BOX = '../data/images/system/unchecked.png'
 
 class PDF(canvas.Canvas):
     output_file: str
@@ -18,6 +24,8 @@ class PDF(canvas.Canvas):
     _styles: Dict[str, Style]
     debug: bool
 
+    _name_index: int
+
     def __init__(self, output_file, styles: Dict, debug: bool = False) -> None:
         pagesize = pagesizes.letter
         super().__init__(output_file, pagesize=pagesize)
@@ -25,10 +33,32 @@ class PDF(canvas.Canvas):
         self.page_height = int(pagesize[1])
         self._styles = styles
         self.debug = debug
+        self._name_index = 0
 
     def finish(self):
         self.showPage()
         self.save()
+
+    def drawImage(self, image, x, y, width=None, height=None, mask=None, preserveAspectRatio=False, anchor='c',
+                  anchorAtXY=False, showBoundary=False):
+        fileName = image.fileName if hasattr(image, 'fileName') else str(image)
+        if fileName == _UNCHECKED_BOX:
+            return self.add_checkbox(x,y,width,height,False)
+        elif fileName == _CHECKED_BOX:
+            return self.add_checkbox(x,y,width,height,True)
+        else:
+            return super().drawImage(image, x, y, width, height, mask, preserveAspectRatio, anchor, anchorAtXY,
+                                 showBoundary)
+
+    def add_checkbox(self, rx, ry, width, height, state) -> (int, int):
+        x,y = self.absolutePosition(rx,ry)
+        self._name_index += 1
+        name = "f%d" % self._name_index
+        LOGGER.debug("Adding checkbox name='%s' with state=%s ", name, state)
+        self.acroForm.checkbox(name=name, x=x-0.5, y=y-0.5, size=min(width, height)+1,
+                               fillColor=reportlab.lib.colors.Color(1,1,1),
+                               buttonStyle = 'cross', borderWidth=0.5, checked=state)
+        return (width, height)
 
     def style(self, style):
         return self._styles[style] if style else None
@@ -51,10 +81,15 @@ class PDF(canvas.Canvas):
             self.setLineWidth(style.borderWidth)
             self.rect(r.left, self.page_height - r.bottom, r.width, r.height, fill=0, stroke=1)
 
-    def draw_flowable(self, paragraph, bounds):
+    def draw_flowable(self, flowable, bounds):
         if self.debug:
             self.stroke_rect(bounds, Style(borderColor=Color('red')))
-        paragraph.drawOn(self, bounds.left, self.page_height - bounds.bottom)
+
+        if hasattr(flowable, 'style'):
+            self._drawing_style = flowable.style
+            LOGGER.debug("Encountered style '%s' with text color =%s", flowable.style.name, flowable.style.textColor)
+
+        flowable.drawOn(self, bounds.left, self.page_height - bounds.bottom)
 
     def make_paragraph(self, run: Run, align=None):
         style = self.style(run.base_style())
@@ -63,8 +98,10 @@ class PDF(canvas.Canvas):
 
         alignment = {'left':0, 'center':1, 'right':2, 'fill':4, 'justify':4}[align]
 
-        pStyle = ParagraphStyle(name='a', fontName=style.font, fontSize=style.size, leading=style.size*1.2,
-                                allowWidows=0, embeddedHyphenation =1, alignment=alignment)
+        pStyle = ParagraphStyle(name='tmp',
+                                fontName=style.font, fontSize=style.size, leading=style.size*1.2,
+                                allowWidows=0, embeddedHyphenation =1, alignment=alignment,
+                                textColor=reportlab.lib.colors.Color(*style.color.rgb))
 
 
         # Add spaces between check boxes and other items
@@ -77,6 +114,7 @@ class PDF(canvas.Canvas):
 
     def descender(self, style: Style) -> float:
         return -pdfmetrics.getDescent(style.font, style.size)
+
 
 
 def _element_to_html(e: Element, pdf: PDF):
@@ -93,6 +131,9 @@ def _element_to_html(e: Element, pdf: PDF):
     size = " size='%d'" % style.size if style.size else ''
     face = " face='%s'" % style.font if style.font else ''
     color = " color='%s'" % style.color.get_hex_l() if style.color else ''
+    if e.which == ElementType.CHECKBOX:
+        target = _UNCHECKED_BOX if e.value in {'O', 'o', ' ', '0'} else _CHECKED_BOX
+        return   "<img height=%d width=%d src='%s'/>" % (style.size, style.size, target)
     if e.which != ElementType.TEXT:
         face = " face='Symbola'"
     return "<font %s%s%s>%s</font>" % (face, size, color, txt)
