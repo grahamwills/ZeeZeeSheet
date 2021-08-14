97a1fe024f7def6a545e8abd5430b6e384565fa7
graham <graham@willsfamily.org>
2021-08-14 16:09:18 -0500

    Placement fix

--- sheet/layout/sheet.py
+++ sheet/layout/sheet.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 
 import functools
 from copy import copy
-from typing import List, Union
+from typing import List
 
-from sheet import common
 from sheet.common import Margins, Rect, configured_logger
 from sheet.layout.block import layout_block
 from sheet.layout.section import stack_in_columns
@@ -21,96 +20,20 @@ def make_block_layout(target: Block, width: int, pdf: PDF) -> PlacedContent:
     return layout_block(target, rect, pdf)
 
 
-class BlockPlacement:
-    target: Block
-    pdf: PDF
-
-    # Placed
-    placed: PlacedContent
-
-    def __init__(self, target: Block, context: PDF):
-        self.pdf = context
-        self.padding = target.padding
-        self.target = target
-
-    def __str__(self):
-        return "§ content=%s" % self.target
-
-    def place(self, bounds: Rect) -> PlacedContent:
-        base = copy(make_block_layout(self.target, bounds.width, self.pdf))
-        base.move(dx=bounds.left - base.actual.left, dy=bounds.top - base.actual.top)
-        # base.requested = bounds
-        self.placed = base
-        return base
-
-    def draw(self):
-        self.placed.draw()
-
-    def __call__(self, bounds: Rect) -> PlacedContent:
-        return self.place(bounds)
-
-
-class _SectionPlacement:
-    target: Union[Section, Sheet]
-    children: List
-    padding: int
-    placed: PlacedContent
-
-    def __init__(self, target: Section, children: List):
-        self.target = target
-        self.children = children
-        self.method = choose_method(target.layout_method)
-
-    def __str__(self):
-        return "%s(%d children)" % (type(self.target).__name__, len(self.children))
-
-    def place(self, bounds: Rect) -> PlacedContent:
-        self.placed = self.method(bounds, self.children, self.target.padding)
-        LOGGER.info("Placed %s: %s", self, self.placed)
-        if hasattr(make_block_layout, 'cache_info'):
-            LOGGER.debug("Cache info = %s", make_block_layout.cache_info())
-            make_block_layout.cache_clear()
-        return self.placed
-
-    def draw(self):
-        self.placed.draw()
-
-
-def choose_method(method: common.Command):
-    if method.command == 'stack':
-        return functools.partial(stack_in_columns, **method.options)
-    else:
-        raise ValueError("unknown layout method for section: '%s'" % method.command)
-
-
-def make_placement(target: Union[Block, Section, Sheet], pdf: PDF):
-    if isinstance(target, Block):
-        return BlockPlacement(target, pdf)
-    else:
-        children = [make_placement(item, pdf) for item in target.content]
-        return _SectionPlacement(target, children)
-
-
-def analyze_placement(p: PlacedContent, depth=0):
-    print('..' * depth, p)
-    print('..' * depth, "  PLACE -> bad=%s, ok=%s, unused=%s, var=%s" %
-          (p.bad_breaks, p.ok_breaks, p.unused_width, p.internal_variance))
-    try:
-        for c in p.group:
-            analyze_placement(c, depth + 1)
-    except:
-        pass
+def place_block(bounds: Rect, block: Block, pdf: PDF) -> PlacedContent:
+    base = copy(make_block_layout(block, bounds.width, pdf))
+    base.move(dx=bounds.left - base.actual.left, dy=bounds.top - base.actual.top)
+    return base
 
 
 def place_section(bounds: Rect, section: Section, pdf: PDF) -> PlacedContent:
-    children = [BlockPlacement(block, pdf) for block in section.content]
+    children = [functools.partial(place_block, block=block, pdf=pdf) for block in section.content]
     placed = stack_in_columns(bounds, children, section.padding, **section.layout_method.options)
     LOGGER.info("Placed %s", section)
     if hasattr(make_block_layout, 'cache_info'):
         LOGGER.debug("Cache info = %s", make_block_layout.cache_info())
         make_block_layout.cache_clear()
     return placed
-
 
 
 def place_sheet(sheet: Sheet, bounds: Rect, pdf: PDF) -> PlacedGroupContent:
